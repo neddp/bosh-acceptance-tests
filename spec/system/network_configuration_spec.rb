a1a30b62fa8b7738a7cae9c14a41fa1ff1c0f9aa
neddp <nedd.petrov@gmail.com>
2025-11-05 07:15:43 +0200

    Introduce nic_group properties

--- spec/system/network_configuration_spec.rb
+++ spec/system/network_configuration_spec.rb
@@ -5,29 +5,19 @@ describe 'network configuration' do
   before(:all) do
     @requirements.requirement(@requirements.stemcell)
     @requirements.requirement(@requirements.release)
-  end
-
-  before(:all) do
     load_deployment_spec
-    use_static_ip
-    use_vip
-    @requirements.requirement(deployment, @spec) # 2.5 min on local vsphere
-  end
-
-  after(:all) do
-    @requirements.cleanup(deployment)
   end
 
   context 'when using manual networking', manual_networking: true do
-    it 'deploys multiple manual networks', multiple_manual_networks: true, ssh: true do
-      use_multiple_manual_networks
-      deployment = with_deployment
-      expect(bosh("-d #{deployment.name} deploy #{deployment.to_path}")).to succeed
+    before(:all) do
+      use_static_ip
+      use_vip
+      @deployment = with_deployment
+      expect(bosh("-d #{@deployment.name} deploy #{@deployment.to_path}")).to succeed
+    end
 
-      expect(bosh_ssh('batlight', 0, 'PATH=/sbin:/usr/sbin:$PATH; ifconfig',
-                      deployment: deployment.name).output).to include(static_ips[0])
-      expect(bosh_ssh('batlight', 0, 'PATH=/sbin:/usr/sbin:$PATH; ifconfig',
-                      deployment: deployment.name).output).to include(static_ips[1])
+    after(:all) do
+      @requirements.cleanup(@deployment)
     end
 
     it 'changes static IP address', changing_static_ip: true, ssh: true do
@@ -35,25 +25,46 @@ describe 'network configuration' do
       deployment = with_deployment
       expect(bosh("-d #{deployment.name} deploy #{deployment.to_path}")).to succeed
 
-      expect(bosh_ssh('batlight', 0, 'PATH=/sbin:/usr/sbin:$PATH; ifconfig',
-                      deployment: deployment.name).output).to include(second_static_ip)
+      expect(bosh_ssh('batlight', 0, 'ip -o addr', deployment: deployment.name, result: true,
+                                                   column: 'stdout').output).to include(second_static_ip)
+    end
+
+    it 'deploys multiple manual networks', multiple_manual_networks: true, ssh: true do
+      use_multiple_manual_networks
+      deployment = with_deployment
+      expect(bosh("-d #{deployment.name} deploy #{deployment.to_path}")).to succeed
+
+      interfaces = bosh_ssh('batlight', 0, 'ip -o addr', deployment: deployment.name, result: true,
+                                                         column: 'stdout').output
+      instance_ips = get_instance_ips
+      expect(instance_ips.values.first.length).to be > 1
+      @spec['properties']['job_networks'].select { |n| n['static_ip'] }.each do |network|
+        expect(interfaces).to include(network['static_ip'])
+      end
     end
   end
 
   context 'when using nic_groups', nic_groups: true, multiple_manual_networks: true do
     before(:all) do
+      load_deployment_spec
+      use_nic_groups
+      use_static_ip
       use_multiple_manual_networks
-      skip "Skipping nic_groups tests because a nic_group is not defined" if nic_groups.empty?
+
+      @deployment = with_deployment
+      expect(bosh("-d #{@deployment.name} deploy #{@deployment.to_path}")).to succeed
     end
+
+    after(:all) do
+      @requirements.cleanup(@deployment)
+    end
+
     it 'assigns networks with the same nic_group to the same interface', ssh: true do
       job_networks = @spec['properties']['job_networks']
       networks_with_nic_groups = job_networks.select { |n| n['static_ip'] && n['nic_group'] }
 
-      deployment = with_deployment
-      expect(bosh("-d #{deployment.name} deploy #{deployment.to_path}")).to succeed
-
-      cli_cmd = 'ip -j addr'
-      output = bosh_ssh('batlight', 0, cli_cmd, deployment: deployment.name, result: true, column: 'stdout').output
+      output = bosh_ssh('batlight', 0, 'ip -j addr', deployment: @deployment.name, result: true,
+                                                     column: 'stdout').output
       raw_interfaces_json = JSON.parse(output)
 
       ip_to_interface_map = {}
@@ -71,7 +82,7 @@ describe 'network configuration' do
         interface_name = ip_to_interface_map[static_ip]
 
         expect(interface_name).not_to be_nil,
-                                 "Static IP #{static_ip} from network '#{network['name']}' not found on any interface"
+                                      "Static IP #{static_ip} from network '#{network['name']}' not found on any interface"
         nic_group_to_interface_set[nic_group] << interface_name
       end
 
